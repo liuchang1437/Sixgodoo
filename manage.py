0c8f70dae89f9592033623b802807ddc2f5e431d
Liu Chang <lc3018121@sjtu.edu.cn>
2016-03-26 15:55:09 +0800

    完善部署（gunicorn manage:app),登陆之前不显示＋与编辑删除按钮

--- manage.py
+++ manage.py
@@ -5,8 +5,10 @@ from app import create_app, db
 from app.models import Item, Blog, Tag, Category
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+#from werkzeug.contrib.fixers import ProxyFix
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+#app.wsgi_app = ProxyFix(app.wsgi_app)
 manager = Manager(app)
 migrate = Migrate(app,db)
 
@@ -22,6 +24,7 @@ def test():
 	import unittest
 	tests = unittest.TestLoader().discover('tests')
 	unittest.TextTestRunner(verbosity=2).run(tests)
+
 
 if __name__ == '__main__':
 	manager.run()
