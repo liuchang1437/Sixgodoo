73fa7c69abde242a72cc25f3d56b3c09ba236ae3
Liu Chang <lc3018121@sjtu.edu.cn>
2016-04-06 17:59:57 +0800

    about页面完成

--- app/main/views.py
+++ app/main/views.py
@@ -231,7 +231,7 @@ def plans():
 	page = request.args.get('page',1,type=int)
 	pagination = Plan.query.order_by(Plan.timestamp.desc()).paginate(page,per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],error_out=False)
 	plans = pagination.items
-	return render_template('plans.html',title=u'Plans',plans=plans,pagination=pagination)
+	return render_template('plans.html',title=u'计划',plans=plans,pagination=pagination)
 
 @main.route('/plans/new_plan',methods=['GET','POST'])
 @login_required
